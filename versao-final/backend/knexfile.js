c81ee5545189cb528d978a1255de448eadee59c1
Leoander Neves <leoander01>
2019-09-23 21:46:10 -0300

    versao-inicial backend

---
+++ versao-final/backend/knexfile.js
@@ -0,0 +1,17 @@
+// const { db } = require('./.env')
+
+module.exports = {
+	client: 'postgresql',
+	connection: {
+		database: `knowledge_final`,
+		user: 'postgres',
+		password: `1234`
+	},
+	pool: {
+		min: 2,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
+};
